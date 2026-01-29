ce8bd416f6d9db21b059771185095ff095e8099e
hy3oning <shyun5635@gmail.com>
2026-01-29 13:53:08 +0900

    [create] BoardServiceImpl create

---
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -0,0 +1,45 @@
+package com.board.service;
+
+import java.util.List;
+
+import com.board.domain.Board;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public int register(Board board) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Board read(Board board) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int modify(Board board) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int remove(Board board) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> list() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Board> search(String searchType, String keyword) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
